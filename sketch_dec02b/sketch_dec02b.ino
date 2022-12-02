43491fd6891649b166b1d87a7a22841a8e8675f3
kimminse3380 <mss2214@naver.com>
2022-12-03 00:24:32 +0900

    add: servo
    하수구 열림과 닫힘 값 넘기는 기능 추가

--- sketch_dec02b/sketch_dec02b.ino
+++ sketch_dec02b/sketch_dec02b.ino
@@ -1,5 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <Servo.h> // servo library  
+#define SensorPin A0  // used for Arduino and ESP8266
+//#define SensorPin 4  // used for ESP32
+Servo s1;
 
 String jsondata = ""; 
 String li = "";
@@ -10,8 +14,13 @@ WiFiClient client;
  JsonObject& root = jsonBuffer.createObject();
    int re = client.connect("192.168.133.227", 80);
 
+void ServoValue(int a) {
+  s1.write(a);
+}
+
 void setup() {
   Serial.begin(115200);
+  s1.attach(0);;
   WiFi.begin("간절한 임베디드", "1234abcd");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -26,13 +35,21 @@ void setup() {
 
 
 void loop() {
+  Serial.println(analogRead(SensorPin));
+  if(analogRead(SensorPin)>400){
+    s1.write(180); 
+    root["openStatus"] = true;
+  }
+  else{
+    s1.write(0); 
+    root["openStatus"] = false;
+  }
   jsondata = "";
-  root["openStatus"] = true;
   root["garbageAmount"] = 30;
   root.printTo(jsondata);
   re = client.connect("192.168.133.227", 80);
   if (re) {
-    client.println("POST /sewer/updateSewerInfo/1 HTTP/1.1");
+    client.println("POST /api/sewer/updateSewerInfo/1 HTTP/1.1");
     client.println("Host: 192.168.133.227");
     client.println("Cache-Control: no-cache");
     client.println("Content-Type: application/json");
