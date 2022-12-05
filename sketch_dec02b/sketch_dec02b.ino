8c0d171574053b2ec7e4f59e11909b0f596c090f
kimminse3380 <mss2214@naver.com>
2022-12-05 13:47:11 +0900

    finish

--- sketch_dec02b/sketch_dec02b.ino
+++ sketch_dec02b/sketch_dec02b.ino
@@ -3,8 +3,16 @@
 #include <Servo.h> // servo library  
 #define SensorPin A0  // used for Arduino and ESP8266
 //#define SensorPin 4  // used for ESP32
-Servo s1;
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
 
+long duration;
+int distanceCm;
+float distanceInch;
+Servo s1;
+Servo s2;
+const int trigPin = 12;
+const int echoPin = 14;
 String jsondata = ""; 
 String li = "";
 int status = WL_IDLE_STATUS;
@@ -14,13 +22,36 @@ WiFiClient client;
  JsonObject& root = jsonBuffer.createObject();
    int re = client.connect("192.168.133.227", 80);
 
-void ServoValue(int a) {
-  s1.write(a);
+void sonarValue(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
 }
 
 void setup() {
   Serial.begin(115200);
-  s1.attach(0);;
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  s1.attach(0);
+  s2.attach(2);
   WiFi.begin("간절한 임베디드", "1234abcd");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -36,16 +67,19 @@ void setup() {
 
 void loop() {
   Serial.println(analogRead(SensorPin));
+  sonarValue();
   if(analogRead(SensorPin)>400){
     s1.write(180); 
+    s2.write(180);
     root["openStatus"] = true;
   }
   else{
-    s1.write(0); 
+    s1.write(0);
+    s2.write(0); 
     root["openStatus"] = false;
   }
   jsondata = "";
-  root["garbageAmount"] = 30;
+  root["garbageAmount"] = distanceCm;
   root.printTo(jsondata);
   re = client.connect("192.168.133.227", 80);
   if (re) {
@@ -61,4 +95,4 @@ void loop() {
     Serial.println(li);
   }
   Serial.println("good");
-}
+} 
